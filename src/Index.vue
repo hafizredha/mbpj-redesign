8a58b6387dd62b03191bbc0c0198317581e68c75
hafizredha <hafizredha@gmail.com>
2025-02-08 00:18:09 +0800

    Services section

--- src/Index.vue
+++ src/Index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import HeroSection from './components/HeroSection.vue'
 import Marquee from './components/Marquee.vue'
+import Service from './components/Service.vue'
 </script>
 
 <template>
   <div>
     <HeroSection />
     <Marquee />
+    <Service />
   </div>
   <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
--- src/components/Marquee.vue
+++ src/components/Marquee.vue
@@ -4,9 +4,9 @@
 
 <template>
     <div class="w-full h-[4dvh] flex">
-        <p class="w-auto h-full bg-red-500 text-[10px] text-white p-2 sm:text-sm flex items-center justify-center font-bold">Announcements</p>
+        <p class="w-auto h-full bg-red-500 text-xs text-white p-2 sm:text-sm flex items-center justify-center font-bold">Announcements</p>
         <div class="w-auto h-full overflow-hidden whitespace-nowrap justify-items-center content-center flex-1">
-            <div class="relative w-full h-full overflow-hidden text-[10px] sm:text-sm py-2 border-b-1 border-[#E6E6E6]">
+            <div class="relative w-full h-full overflow-hidden text-xs sm:text-sm py-2 border-b-1 border-[#E6E6E6]">
                 <div class="flex w-full h-full animate-slideText text-left font-bold items-center">
                     <a href="#" class="min-w-full px-2 truncate hover:underline">Mobile Counter Schedule for Year 2025 License Renewal</a>
                     <a href="#" class="min-w-full px-2 truncate hover:underline">Spring cleaning schedule for Hari Raya Aidil Fitri 2025</a>
