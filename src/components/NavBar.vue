599618972b7f326df0f81ee7579046996b58caf8
hafizredha <hafizredha@gmail.com>
2025-02-14 17:31:50 +0800

    Navbar changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -19,23 +19,28 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <header class="fixed top-0 left-0 right-0 p-[inherit] border-b-1 border-gray-200/20 md:border-none transition-all z-99999 overflow-hidden" 
+    <!-- Overlay -->
+    <div 
+        class="fixed inset-0 bg-black transition-opacity duration-500 ease-in-out md:hidden z-999"
+        :class="isOpen ? 'opacity-80 pointer-events-auto' : 'opacity-0 pointer-events-none'"
+        @click="isOpen = false"
+    ></div>
+    <header class="fixed top-0 left-0 right-0 p-[inherit] border-b-1 border-gray-200/20 transition-all z-99999 overflow-hidden" 
     :class="[
-        isOpen ? 'bg-white duration-100 ease-in' : 'bg-transparent duration-600 delay-300 ease-out',
-        isScrolled ? 'bg-white shadow-md duration-300 ease-in' : 'bg-transparent duration-600 delay-300 ease-out'
+        isOpen || isScrolled ? 'bg-white duration-100 ease-in' : 'bg-transparent duration-600 delay-300 ease-out',
     ]">
         <div class="max-w-6xl m-auto flex justify-between items-center">
             <div class="flex gap-3 items-center">
                 <img class="h-[40px] w-auto duration-600 delay-300 ease-out" :class="isScrolled ? 'sm:h-[40px]' : 'sm:h-[60px]'" src="/assets/logo-mbpj.png"/>
                 <img class="h-[40px] sm:h-[70px] w-auto md:hidden" src="/assets/logo_selangor.png"/>
                 
-                <p class="text-xs sm:text-lg font-medium duration-600 delay-300 ease-out" :class="isScrolled ? 'text-black' : ' text-white'">Petaling Jaya Municipality</p>
+                <p class="text-xs sm:text-lg md:hidden lg:block font-medium duration-600 delay-300 ease-out" :class="isScrolled || isOpen ? 'text-black' : ' text-white'">Petaling Jaya Municipality</p>
             </div>
             <div class="flex items-center gap-5">
                 <nav class="top-[72px] sm:top-[102px] md:top-0 md:relative md:max-h-full! fixed inset-x-0 bg-white md:bg-transparent rounded-b-xl overflow-hidden md:overflow-visible transition-all duration-500 ease-in-out"
                 :class="isOpen ? 'max-h-screen' : 'max-h-0'">
                     <ul class="flex flex-col md:flex-row p-3 md:p-0 gap-2 text-left text-base text-black/80 font-medium border-t-1 border-black-500 md:border-none duration-600 delay-300 ease-out"
-                    :class="isScrolled ? 'text-black/80' : ' text-white'">
+                    :class="isScrolled ? 'md:text-black/80' : 'md:text-white'">
                         <li>
                             <a class="block p-2" href="#">Home</a>
                         </li>
